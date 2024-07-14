48054d7541787722d5baceb693cb4a272050134e
Shubham Gupta <shubham.guptak07@gmail.com>
2024-07-15 01:21:06 +0530

    Updated for Calling doPayment API with Feign Client from placeOrder

---
+++ src/main/java/com/demo/orderservice/external/client/PaymentService.java
@@ -0,0 +1,13 @@
+package com.demo.orderservice.external.client;
+
+import com.demo.orderservice.external.request.PaymentRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "PAYMENT-SERVICE/payment")
+public interface PaymentService {
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+}
--- src/main/java/com/demo/orderservice/service/OrderServiceImpl.java
+++ src/main/java/com/demo/orderservice/service/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.demo.orderservice.service;
 
 import com.demo.orderservice.entity.Order;
+import com.demo.orderservice.external.client.PaymentService;
 import com.demo.orderservice.external.client.ProductService;
+import com.demo.orderservice.external.request.PaymentRequest;
 import com.demo.orderservice.model.OrderRequest;
 import com.demo.orderservice.respository.OrderRespository;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @Log4j2
@@ -17,6 +20,8 @@ public class OrderServiceImpl implements OrderService {
     private OrderRespository orderRespository;
     @Autowired
     private ProductService productService;
+    @Autowired
+    private PaymentService paymentService;
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
@@ -31,6 +36,24 @@ public class OrderServiceImpl implements OrderService {
                 .orderTime(Instant.now())
                 .quantity(orderRequest.getQuantity())
                 .build();
+        orderRespository.save(order);
+        log.info("Calling Payment service to complete the payment");
+        PaymentRequest  paymentRequest=PaymentRequest.builder()
+                .orderId(order.getOrderId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .amount(orderRequest.getTotalAmount())
+                .referenceNumber(String.valueOf(UUID.randomUUID()))
+                .build();
+        String orderStatus=null;
+        try {
+            paymentService.doPayment(paymentRequest);
+            log.info("Payment done successfully. Changing the order status ");
+            orderStatus="PLACED";
+        }catch (Exception e){
+            log.error("Error occured in payment. Changing status to PAYMENT_FAILED");
+            orderStatus="PAYMENT_FAILED";
+        }
+        order.setOrderStatus(orderStatus);
         orderRespository.save(order);
         log.info("Order placed with order id : {}", order.getOrderId());
         return order.getOrderId();
